7adadafaf7afecc45adb8a97f0cb56635859dc80
gunhawk <gunhawk998@gmail.com>
2020-05-22 06:00:04 +0800

    fenhance functions

--- codes/addEvent/index.js
+++ codes/addEvent/index.js
@@ -1,8 +1,31 @@
 function addEvent(elem, evnt, func) {
-  if (elem.addEventListener) elem.addEventListener(evnt, func, false)
-  else if (elem.attachEvent) {
-    elem.attachEvent('on' + evnt, func)
+  if (elem.addEventListener) return elem.addEventListener(evnt, func, false);
+
+  const wrap = function (e) {
+    const event = e || window.event;
+
+    if (!event.preventDefault) {
+      event.preventDefault = function () {
+        event.returnValue = false;
+      };
+    }
+
+    if (!event.stopPropagation) {
+      event.stopPropagation = function () {
+        event.cancelBubble = true;
+      };
+    }
+
+    if (!event.target) {
+      event.target = event.srcElement;
+    }
+
+    return func.call(this, event);
+  };
+
+  if (elem.attachEvent) {
+    elem.attachEvent('on' + evnt, wrap);
   } else {
-    elem['on' + evnt] = func
+    elem['on' + evnt] = wrap;
   }
 }
